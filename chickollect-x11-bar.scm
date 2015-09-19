696b4e73d09fe991bbaa712ffac1719d69dd0a8b
Mario Domenech Goulart <mario.goulart@gmail.com>
2015-09-19 18:41:41 -0300

    Use individual parameters for configuration
    Depend on chickollect's keyword parameter API.

--- chickollect-x11-bar.scm
+++ chickollect-x11-bar.scm
@@ -1,9 +1,19 @@
 (use data-structures extras posix)
 (use ezxdisp chickollect)
 
-(define default-height 12)
+(define bar-height (make-parameter 12))
+(define bar-width (make-parameter 800))
+(define bar-title (make-parameter "chickollect-x11-bar"))
+(define bar-background-color (make-parameter (make-ezx-color 0 0 0)))
+(define bar-text-color (make-parameter (make-ezx-color 0.4 0.4 0.4)))
+(define collect-hook (make-parameter #f))
+(define network-devices (make-parameter '()))
 
-(define config (make-parameter '()))
+;; Parameters for chickollect
+(define time-format (make-parameter "%T"))
+(define date-format (make-parameter "%F"))
+(define collect-interval (make-parameter 1))
+(define monitors (make-parameter '(memory cpu date time battery network)))
 
 (define (trunc n)
   (inexact->exact (truncate n)))
@@ -32,10 +42,7 @@
          (conc " " n))
         (else (->string n))))
 
-(define (config-val field #!optional default)
-  (alist-ref field (config) eqv? default))
-
-(define (format-network-devices sys-data network-devices)
+(define (format-network-devices sys-data)
   (string-intersperse
    (map (lambda (dev)
           (or (and-let* ((net-conf (alist-ref 'network sys-data))
@@ -47,7 +54,7 @@
                                      download
                                      upload)))
               (symbol->string dev)))
-        network-devices)
+        (network-devices))
    "  |  "))
 
 (define (format-batteries batteries-data)
@@ -68,67 +75,52 @@
          batteries-data)
     " ")))
 
-(define redraw
-  (let ((conf-parsed? #f)
-        (height #f)
-        (text-color #f)
-        (network-devices #f))
-    (lambda (bar sys-data reparse-conf?)
-      (when (or reparse-conf? (not conf-parsed?))
-        (set! network-devices (config-val 'network-devices '(eth0)))
-        (set! text-color (apply make-ezx-color
-                                (config-val 'text-color '(1 1 1))))
-        (set! height (config-val 'bar-height default-height))
-        (set! conf-parsed? #t))
-      (unless reparse-conf?
-        (let* ((memory (alist-ref 'memory sys-data))
-               (ram (trunc (car memory)))
-               (swap (trunc (cdr memory)))
-               (batteries-data (alist-ref 'battery sys-data))
-               (cpu-stats (alist-ref 'cpu sys-data))
-               (num-cpus (length cpu-stats))
-               (avg-cpu-usage (trunc (/ (apply + cpu-stats) num-cpus)))
-               (bar-fmt
-                "~a  ~a  |  CPU: ~a%  |  RAM: ~a%  |  SWAP: ~a%  |  ~a  |  ~a")
-               (content (sprintf bar-fmt
-                                 (alist-ref 'time sys-data)
-                                 (alist-ref 'date sys-data)
-                                 (pad-number avg-cpu-usage)
-                                 (pad-number ram)
-                                 (pad-number swap)
-                                 (format-batteries batteries-data)
-                                 (format-network-devices sys-data network-devices))))
-          (ezx-wipe bar)
-          (ezx-str-2d bar 2 (fx- height 1) content text-color)
-          (ezx-redraw bar))))))
+(define (redraw bar sys-data)
+  (let* ((memory (alist-ref 'memory sys-data))
+         (ram (trunc (car memory)))
+         (swap (trunc (cdr memory)))
+         (batteries-data (alist-ref 'battery sys-data))
+         (cpu-stats (alist-ref 'cpu sys-data))
+         (num-cpus (length cpu-stats))
+         (avg-cpu-usage (trunc (/ (apply + cpu-stats) num-cpus)))
+         (bar-fmt
+          "~a  ~a  |  CPU: ~a%  |  RAM: ~a%  |  SWAP: ~a%  |  ~a  |  ~a")
+         (content (sprintf bar-fmt
+                           (alist-ref 'time sys-data)
+                           (alist-ref 'date sys-data)
+                           (pad-number avg-cpu-usage)
+                           (pad-number ram)
+                           (pad-number swap)
+                           (format-batteries batteries-data)
+                           (format-network-devices sys-data))))
+    (ezx-wipe bar)
+    (ezx-str-2d bar 2 (fx- (bar-height) 1) content (bar-text-color))
+    (ezx-redraw bar)))
 
 (define (main args)
   (let ((conf-file (and (not (null? args))
                         (car args))))
     (when conf-file
       (load conf-file))
-    (let* ((width (config-val 'bar-width 800))
-           (title (config-val 'bar-title "chickollect-x11-bar"))
-           (height (config-val 'bar-height default-height))
-           (bg-color (apply make-ezx-color
-                            (config-val 'background-color '(0 0 0))))
-           (bar (ezx-init width height title))
-           (collect-hook (config-val 'hook)))
+    (let ((bar (ezx-init (bar-width) (bar-height) (bar-title))))
 
       ;; Reload configuration upon receiving SIGHUP
-      (set-signal-handler! signal/hup
-                           (lambda (signum)
-                             (load conf-file)
-                             (set! collect-hook (config-val 'hook))
-                             (redraw bar '() #t)))
+      (set-signal-handler!
+       signal/hup
+       (lambda (signum)
+         (load conf-file)
+         (redraw bar '())))
 
-      (ezx-set-background bar bg-color)
+      (ezx-set-background bar (bar-background-color))
       (collect-loop
        (lambda (data)
-         (when collect-hook
-           (collect-hook data))
-         (redraw bar data #f))
-       conf: (config))
+         (when (collect-hook)
+           ((collect-hook) data))
+         (redraw bar data))
+       time-format: (time-format)
+       date-format: (date-format)
+       collect-interval: (collect-interval)
+       monitors: (monitors))
       (ezx-quit bar))))
 
 (main (command-line-arguments))
